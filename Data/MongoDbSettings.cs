0cbe0e1621e6f0cbdf23c46f3a8ebbec54e6e4ca
Vagelis Oikonomou <vagelis.ikon@gmail.com>
2026-01-30 19:21:35 +0200

    Delete Data directory

--- Data/MongoDbSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MiniEshopBlazor.Data
-{
-    public class MongoDbSettings
-    {
-        public string ConnectionString { get; set; }
-        public string DatabaseName { get; set; }
-        public string ProductsCollectionName { get; set; }
-    }
-}
